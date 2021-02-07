19b8b2567f1cd633dcf5052dc0316244d6e772c5
Nouf213 <noufshoubaki@gmail.com>
2021-02-07 16:50:43 +0200

    new addition to js

---
+++ app.js
@@ -0,0 +1,10 @@
+var fullname = 'nouf';
+console.log("full name:"+fullname);
+var age = prompt("enter your age");
+console.log (age);
+
+if (age>30) {
+    alert("not student")
+} else {
+    alert("student")
+};
