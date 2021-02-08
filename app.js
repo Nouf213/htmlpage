147b2a733eb8e1dbffda1c6bda5affec1ef15ab6
nouf <noufshoubaki@gmail.com>
2021-02-08 14:00:42 +0200

    new js commands

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ if (age>30) {
 } else {
     alert("student")
 };
+
+
+var order=prompt("what you want to eat Dessert or launch?");
+
+while (order !== "dessert")
+ {
+    order=prompt("what your want to eat desset or launch?");
+}
+
+var itemorder='';
+if (order == "dessert") {
+    itemorder = '<img src= "leverocks-dessert-menu-2019.jpg"/>';
+} 
+else if (order == "launch")
+ { itemorder= '<img src= "lunch.jpg">';
+    
+} 
+
+var numofdesserts=prompt ("what is the number of desserts ?");
+var result='';
+for (var i=0; i<numofdesserts;i++)
+{
+    result=result+itemorder;
+}
+
+document.write (result);
